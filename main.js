b4af4435d2400598bcf4c32f6102aef637aebd72
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-03-13 17:37:16 -0600

    var, let and const

---
+++ main.js
@@ -0,0 +1,38 @@
+/*console.log(test); //returns undifined beacuse run the declaretion before be declared
+var test = 'test';*/
+
+/*console.log(test); //returns an error beacuse we cannot access 'test' before initialization nad doesnt have hoisting
+let test = 'test';*/
+
+/*console.log(test); //returns an error beacuse we cannot access 'test' before initialization nad doesnt have hoisting
+const test = 'test';*/
+
+/*test();//hoisting with function
+function test(){
+    console.log('im a function')
+}*/
+
+/*
+function test() {// scope example
+    var nice = 'nice'
+}
+console.log(nice);*/
+
+/*if (true) {// block scope - change var to let/const
+    var test = 'test';
+}
+console.log(test);*/
+
+
+/*for(var i=0; i<3; i++) {// for loop example - to fix this use let
+    setTimeout(() => {
+        alert(i);
+    }, 500);
+}*/
+
+
+for(var i=0; i<3; i++) {// use function factory
+    (function test(o){
+        alert(o);
+    })(i)
+}
