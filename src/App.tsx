4f412a6b8fd38517a0bf3ba578259d6d42be7870
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-03-13 20:51:53 -0600

    Component & Props

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <>
+      <h1>My App</h1>
+      <Button type="primary">Primary</Button>
+      <Button>Default</Button>
+    </>
+  );
+}
+
+export default App;
